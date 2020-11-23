299b4bfc02285f46af3f281683b89b951660d965
김현아 <hyuna@gimhyeon-aui-MacBookPro.local>
2020-11-23 14:42:19 +0900

    activity

--- app/src/main/java/com/example/kusitms/SplashActivity.kt
+++ app/src/main/java/com/example/kusitms/SplashActivity.kt
@@ -3,13 +3,20 @@ package com.example.kusitms
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import java.util.logging.Handler as Handler1
 
 class SplashActivity : AppCompatActivity() {
 
+    private val SPLASH_TIME_OUT: Long = 2000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
-        finish()
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }, SPLASH_TIME_OUT)
     }
 }
