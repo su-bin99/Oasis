26238ba1b06b102f102686beff1781a170842457
su-bin99 <sbsoobin@naver.com>
2020-11-24 21:56:21 +0900

    front

--- app/src/main/java/com/example/kusitms/personTab/Adapter_Person.kt
+++ app/src/main/java/com/example/kusitms/personTab/Adapter_Person.kt
@@ -1,8 +1,11 @@
 package com.example.kusitms.personTab
 
+import android.media.Image
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.kusitms.R
@@ -16,8 +19,15 @@ class Adapter_Person(options: FirebaseRecyclerOptions<Data_Person>) :
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var pWriterText : TextView = itemView.findViewById(R.id.pWriterText)
         var pSubjectText : TextView = itemView.findViewById(R.id.pSubjectText)
-        var pTagText : TextView = itemView.findViewById(R.id.pTagText)
-
+        var pTagText1 : TextView = itemView.findViewById(R.id.pTagText1)
+        var pTagText2 : TextView = itemView.findViewById(R.id.pTagText2)
+        var pTagText3 : TextView = itemView.findViewById(R.id.pTagText3)
+        var pTagTextArray = arrayListOf<TextView>(pTagText1, pTagText2, pTagText3)
+        var imageView : ImageView = itemView.findViewById(R.id.personImg)
+        var pTagLayout1 : LinearLayout =  itemView.findViewById(R.id.pTagLayout1)
+        var pTagLayout2 : LinearLayout =  itemView.findViewById(R.id.pTagLayout2)
+        var pTagLayout3 : LinearLayout =  itemView.findViewById(R.id.pTagLayout3)
+        var pTagLayoutArray = arrayListOf<LinearLayout>(pTagLayout1, pTagLayout2, pTagLayout3)
 
         init {
             itemView.setOnClickListener {
@@ -40,11 +50,16 @@ class Adapter_Person(options: FirebaseRecyclerOptions<Data_Person>) :
     override fun onBindViewHolder(holder: ViewHolder, position: Int, model: Data_Person) {
         holder.pWriterText.text = model.person_writer
         holder.pSubjectText.text = model.person_subject
-        var tag= ""
-        for ( i in model.person_tag){
-            tag += "#"+i
+
+        for ( i in 0..2 ){
+            if(i < model.person_tag.size){
+                holder.pTagTextArray[i].text = model.person_tag[i]
+            }else{
+                holder.pTagTextArray[i].visibility = View.INVISIBLE
+                holder.pTagLayoutArray[i].setBackgroundResource(R.drawable.back_none)
+            }
         }
-        holder.pTagText.text = tag
+
     }
 
 }
