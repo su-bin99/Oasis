5f3c59110dfa552eaa7701dd1730b34192c1c134
김현아 <hyuna@gimhyeon-aui-MacBookPro.local>
2020-11-25 20:29:13 +0900

    data

--- app/src/main/java/com/example/kusitms/activityTab/Data_Activity.kt
+++ app/src/main/java/com/example/kusitms/activityTab/Data_Activity.kt
@@ -1,20 +1,20 @@
 package com.example.kusitms.activityTab
 
 data class Data_Activity(
-    var activity_concept : ArrayList<String>,
-    var activity_field : ArrayList<String>,
-    var activity_id : Int,
     var activity_type : String,
-    var content : String,
-    var maxPeoplenum : Int,
-    var participate : ArrayList<String>,
-    var pic_url : String,
-    var subject : String,
-    var time : String,
-    var writer : String
+    var activity_field : ArrayList<String>,
+    var activity_content : String,
+    var activity_maxPeoplenum : Int,
+    var activity_pic_url : String,
+    var activity_object : String,
+    var activity_participate : ArrayList<String>,
+    var activity_subject : String,
+    var activity_time : String,
+    var activity_writer : String,
+    var activity_uid : String
 ) {
     constructor():
-            this(ArrayList<String>(), ArrayList<String>(),
-                0, "", "", 0,
+            this("", ArrayList<String>(),
+                 "", 0, "","",
                 ArrayList<String>(), "", "", "", "")
 }
--- app/src/main/java/com/example/kusitms/activityTab/WriteActivity_Activity.kt
+++ app/src/main/java/com/example/kusitms/activityTab/WriteActivity_Activity.kt
@@ -7,7 +7,10 @@ import android.util.Log
 import android.widget.Toast
 import com.example.kusitms.R
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_write.*
 import java.text.SimpleDateFormat
@@ -17,71 +20,94 @@ import kotlin.collections.ArrayList
 class WriteActivity_Activity : AppCompatActivity() {
 
     val user = Firebase.auth.currentUser
-    val uid = user?.uid
+    val userid = user?.uid
 
-    var concept = ""
-    var field = ""
+
     var type = ""
-    var subject = ""
-    var MaxPeopleNum = 0
+    var field = ""
     var content = ""
+    var subject = ""
+    var maxPeopleNum = 0
     var pic_url = ""
-    var userId = ""
+    var act_object = ""
+    var participate = ""
+    var uid = userid.toString()
+    var value = ""
+
+    var myRef = FirebaseDatabase.getInstance().reference.child("my_page").child(uid).child("privacy").child("name")
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_write)
+        myRef.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                value = snapshot.value.toString()
+                println("did you")
+                println(value)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                println("failed to read value")
+            }
+        })
+    }
+
+    override fun onStart() {
+        super.onStart()
         init()
     }
+
     fun init(){
         activity_submitBtn.setOnClickListener {
-            var conceptarray = ArrayList<String>()
             var fieldarray = ArrayList<String>()
+            var participatearray = ArrayList<String>()
 
-            concept = activity_concept.text.toString()
             field = activity_field.text.toString()
             type = activity_type.text.toString()
             subject = activity_subject.text.toString()
             var temp = activity_maxPeopleNum.text.toString()
             content = activity_content.text.toString()
-            userId = uid.toString()
+            participate = activity_participate.text.toString()
 
-            conceptarray.add(concept)
+
             fieldarray.add(field)
+            participatearray.add(participate)
 
             try {
-                MaxPeopleNum = temp.toInt()
+                maxPeopleNum = temp.toInt()
             } catch (e: NumberFormatException) {
                 Log.d(ContentValues.TAG, temp + "는 숫자가 아님 ");
             }
 
+            var writer = value
+
             val currentDateTime = Calendar.getInstance().time
             var time = SimpleDateFormat("yyyy.MM.dd HH:mm", Locale.KOREA).format(currentDateTime)
-            insertData(conceptarray, fieldarray, 0, type, content, MaxPeopleNum,
-                pic_url, subject, time, userId )
+            insertData(type, fieldarray, content,0, pic_url, act_object, participatearray,
+                subject, time, writer, uid)
         }
     }
 
-    fun insertData(activity_concept : ArrayList<String>,
+    fun insertData(activity_type : String,
                    activity_field : ArrayList<String>,
-                   activity_id : Int,
-                   type : String,
-                   content : String,
-                   maxPeoplenum : Int,
-                   pic_url : String,
-                   subject : String,
-                   time : String,
-                   writer : String){
+                   activity_content : String,
+                   activity_maxPeoplenum : Int,
+                   activity_pic_url : String,
+                   activity_object : String,
+                   activity_participate : ArrayList<String>,
+                   activity_subject : String,
+                   activity_time : String,
+                   activity_writer : String,
+                   activity_uid : String){
         var dataRef = FirebaseDatabase.getInstance().reference.child("activity").push()
 
-        for(i in 0 until activity_concept.size){
-            dataRef.child("activity_concept").child(i.toString()).setValue(concept)
-        }
-
+        dataRef.child("activity_type").setValue(activity_type)
         for(i in 0 until activity_field.size){
             dataRef.child("activity_field").child(i.toString()).setValue(field)
         }
-
+        dataRef.child("activity_content").setValue(activity_content)
+/*
         dataRef.child("activity_id").setValue(activity_id)
             .addOnSuccessListener {
                 Log.d(ContentValues.TAG, "activity_id저장성공 ");
@@ -89,13 +115,19 @@ class WriteActivity_Activity : AppCompatActivity() {
             .addOnFailureListener{
                 Log.d(ContentValues.TAG, "activity_id저장실패 ");
             }
-        dataRef.child("activity_type").setValue(type)
-        dataRef.child("content").setValue(content)
-        dataRef.child("maxPeoplenum").setValue(maxPeoplenum)
-        dataRef.child("pic_url").setValue(pic_url)
-        dataRef.child("subject").setValue(subject)
-        dataRef.child("time").setValue(time)
-        dataRef.child("writer").setValue(writer)
+
+ */
+        dataRef.child("activity_maxPeoplenum").setValue(activity_maxPeoplenum)
+        dataRef.child("activity_pic_url").setValue(activity_pic_url)
+        dataRef.child("activity_object").setValue(activity_object)
+        for(i in 0 until activity_participate.size){
+            dataRef.child("activity_participate").child(i.toString()).setValue(activity_participate)
+        }
+
+        dataRef.child("activity_subject").setValue(activity_subject)
+        dataRef.child("activity_time").setValue(activity_time)
+        dataRef.child("activity_writer").setValue(activity_writer)
+        dataRef.child("activity_uid").setValue(activity_uid)
 
         val myToast = Toast.makeText(this.applicationContext, "글 작성이 완료되었습니다.", Toast.LENGTH_SHORT)
         myToast.show()
