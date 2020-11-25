84839862136a2fd0cdb1f039e8036c3c0900e430
jeohyn <chajhyeon@gmail.com>
2020-11-25 21:49:20 +0900

    Merge branch 'master' of C:\Users\chaj9\AndroidStudioProjects\kusitms-MainService with conflicts.

--- app/src/main/java/com/example/kusitms/activityTab/Adapter_Activity.kt
+++ app/src/main/java/com/example/kusitms/activityTab/Adapter_Activity.kt
@@ -1,15 +1,10 @@
 package com.example.kusitms.activityTab
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.resource.bitmap.RoundedCorners
-import com.bumptech.glide.request.RequestOptions
 import com.example.kusitms.R
 import com.firebase.ui.database.FirebaseRecyclerAdapter
 import com.firebase.ui.database.FirebaseRecyclerOptions
@@ -19,11 +14,12 @@ class Adapter_Activity(options: FirebaseRecyclerOptions<Data_Activity>) :
     //options : 쿼리가 들어가는것 / 어떤 질의에 대한 어댑터냐
 
     var itemClickListener : OnItemClickListener?= null
-    var context : Context?= null
 
     inner class ViewHolder(itemView : View): RecyclerView.ViewHolder(itemView){
+        var writerText : TextView = itemView.findViewById(R.id.writerText)
         var subjectText : TextView = itemView.findViewById(R.id.subjectText)
-        var imageView : ImageView = itemView.findViewById(R.id.activityImg)
+        var typeText : TextView = itemView.findViewById(R.id.activityTypeText)
+        var activityIdText : TextView = itemView.findViewById(R.id.activityIdText)
 
         init{
             itemView.setOnClickListener{
@@ -40,16 +36,18 @@ class Adapter_Activity(options: FirebaseRecyclerOptions<Data_Activity>) :
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val v = LayoutInflater.from(parent.context)
             .inflate(R.layout.row_activity, parent, false)
-        context = parent.getContext()
         return ViewHolder(v)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int, model: Data_Activity) {
-        holder.subjectText.text = model.activity_subject
-        Glide.with(context!!).load(R.drawable.place_img)
-            .apply(RequestOptions.bitmapTransform(RoundedCorners(10)))
-            .override(360, 170)
-            .into(holder.imageView);
+        holder.writerText.text = model.writer
+        holder.subjectText.text = model.subject
+        holder.activityIdText.text = model.activity_id.toString()
+        var type = ""
+        for ( i in model.activity_type){
+            type += " #" + i
+        }
+        holder.typeText.text = type
 
     }
 }
